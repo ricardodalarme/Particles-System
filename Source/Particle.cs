b402b072cac37239c57c94ae8a71972b815d92f7
ricardodalarme <ricardoweasley@hotmail.com>
2020-09-06 16:54:37 -0300

    Created Particle class

---
+++ Source/Particle.cs
@@ -0,0 +1,12 @@
+﻿namespace Particles
+{
+    class Particle
+    {
+        public int StartLife { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double SpeedX { get; set; }
+        public double SpeedY { get; set; }
+
+    }
+}
