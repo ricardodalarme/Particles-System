11d4eca3c64b683d2b7bdd465878a6006a67c4e9
ricardodalarme <ricardoweasley@hotmail.com>
2020-09-07 13:13:32 -0300

    Changed Windows.Forms to DarkUI.Forms

--- Source/Editor.Designer.cs
+++ Source/Editor.Designer.cs
@@ -28,33 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.darkLabel1 = new DarkUI.Controls.DarkLabel();
+            this.darkTextBox1 = new DarkUI.Controls.DarkTextBox();
             this.picParticle = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.picParticle)).BeginInit();
             this.SuspendLayout();
             // 
+            // darkLabel1
+            // 
+            this.darkLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.darkLabel1.Location = new System.Drawing.Point(12, 11);
+            this.darkLabel1.Name = "darkLabel1";
+            this.darkLabel1.Size = new System.Drawing.Size(60, 13);
+            this.darkLabel1.TabIndex = 0;
+            this.darkLabel1.Text = "Life time:";
+            // 
+            // darkTextBox1
+            // 
+            this.darkTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(73)))), ((int)(((byte)(74)))));
+            this.darkTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.darkTextBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.darkTextBox1.Location = new System.Drawing.Point(15, 27);
+            this.darkTextBox1.Name = "darkTextBox1";
+            this.darkTextBox1.Size = new System.Drawing.Size(193, 20);
+            this.darkTextBox1.TabIndex = 1;
+            // 
             // picParticle
             // 
-            this.picParticle.Location = new System.Drawing.Point(381, 12);
+            this.picParticle.Location = new System.Drawing.Point(224, 12);
             this.picParticle.Name = "picParticle";
-            this.picParticle.Size = new System.Drawing.Size(352, 374);
-            this.picParticle.TabIndex = 0;
+            this.picParticle.Size = new System.Drawing.Size(400, 400);
+            this.picParticle.TabIndex = 2;
             this.picParticle.TabStop = false;
             // 
             // Editor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(635, 422);
             this.Controls.Add(this.picParticle);
+            this.Controls.Add(this.darkTextBox1);
+            this.Controls.Add(this.darkLabel1);
+            this.MaximizeBox = false;
             this.Name = "Editor";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Particles Editor";
             ((System.ComponentModel.ISupportInitialize)(this.picParticle)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
+        private DarkUI.Controls.DarkLabel darkLabel1;
+        private DarkUI.Controls.DarkTextBox darkTextBox1;
         public System.Windows.Forms.PictureBox picParticle;
     }
 }
--- Source/Editor.cs
+++ Source/Editor.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using DarkUI.Forms;
 
 namespace Particles
 {
-    public partial class Editor : Form
+    public partial class Editor : DarkForm
     {
         public Editor()
         {
