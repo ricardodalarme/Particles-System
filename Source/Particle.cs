688a1b0cb072ec6333232bf6b755974ddfa3a598
ricardodalarme <ricardoweasley@hotmail.com>
2020-10-25 19:23:55 -0300

    Particle color

--- Source/Particle.cs
+++ Source/Particle.cs
@@ -1,10 +1,48 @@
-﻿using SFML.System;
+﻿using SFML.Graphics;
+using SFML.System;
+using System;
+using static Particles.Program;
 
 namespace Particles
 {
-    class Particle
+    class Particle 
     {
         public Vector2f Velocity;
         public Time LifeTime = Time.Zero;
+        public Vertex Vertices;
+        private readonly ParticlePool Pool;
+
+        public Particle(ParticlePool pool)
+        {
+            Pool = pool;
+        }
+
+        public void Update(Time elapsed)
+        {
+            LifeTime -= elapsed;
+
+            // if the particle is dead, respawn it
+            if (LifeTime <= Time.Zero)
+                Reset();
+
+            // update the alpha (transparency) of the particle according to its lifetime
+            float ratio = LifeTime.AsMilliseconds() / Pool.LifeTime;
+            Vertices = new Vertex(
+                Vertices.Position + Velocity * elapsed.AsSeconds(),
+                    new Color(Pool.R, Pool.G, Pool.B, (byte)(ratio * 255))
+                );
+        }
+
+        public void Reset()
+        {
+            // give a random velocity and lifetime to the particle
+            double angle = (Randomizer.Next(0, 360)) * 3.14f / 180.0;
+            double speed = Randomizer.Next(0, 50) + Pool.Speed;
+            Velocity = new Vector2f((float)(Math.Cos(angle) * speed), (float)(Math.Sin(angle) * speed));
+            LifeTime = Time.FromMilliseconds((Randomizer.Next(0, Pool.LifeTime)) + 1000);
+
+            // reset the position of the corresponding vertex
+            Vertices = new Vertex(Pool.Emitter);
+        }
     }
 }
--- Source/ParticlePool.cs
+++ Source/ParticlePool.cs
@@ -1,16 +1,34 @@
 ﻿using SFML.Graphics;
 using SFML.System;
-using System;
 using System.Collections.Generic;
-using static Particles.Program;
+using System.ComponentModel;
 
 namespace Particles
 {
     class ParticlePool : Drawable
     {
-        // Pool properties
+        // Pool propertiess
         public int Speed { get; set; }
-        public int Count { get; set; }
+
+        // Cor da partícula
+        [Category("Color")]
+        public byte R { get; set; }
+        [Category("Color")]
+        public byte G { get; set; }
+        [Category("Color")]
+        public byte B { get; set; }
+
+        public int Count
+        {
+            get => Particles.Capacity; 
+            set
+            {
+                Particles.Clear();
+                Particles.Capacity = value;
+                for (var i = 0; i < Count; i++) Particles.Add(new Particle(this));
+            }
+        }
+
         public int LifeTime
         {
             get => lifeTime.AsMilliseconds();
@@ -23,56 +41,23 @@ namespace Particles
             Speed = 50;
             Count = 1000;
             LifeTime = 100;
-
-
-            Vertices = new VertexArray(PrimitiveType.Points, (uint)Count);
-            for (var i = 0; i < Vertices.VertexCount; i++) Particles.Add(new Particle());
         }
 
         // Pool informations
         public Time lifeTime;
         public Vector2f Emitter = new Vector2f(0f, 0f);
         public List<Particle> Particles = new List<Particle>();
-        public VertexArray Vertices;
 
         public void Update(Time elapsed)
         {
-            for (int i = 0; i < Particles.Count; ++i)
-            {
-                // update the particle lifetime
-                Particle p = Particles[i];
-                p.LifeTime -= elapsed;
-
-                // if the particle is dead, respawn it
-                if (p.LifeTime <= Time.Zero)
-                    ResetParticle(i);
-
-                // update the alpha (transparency) of the particle according to its lifetime
-                float ratio = p.LifeTime.AsSeconds() / lifeTime.AsSeconds();
-                Vertices[(uint)i] = new Vertex(
-                    Vertices[
-                        (uint)i].Position + p.Velocity * elapsed.AsSeconds(),
-                        new Color(220, 255, 255, (byte)(ratio * 255))
-                    );
-            }
-        }
-
-        public void ResetParticle(int index)
-        {
-            // give a random velocity and lifetime to the particle
-            double angle = (Randomizer.Next(0, 360)) * 3.14f / 180.0;
-            double speed = Randomizer.Next(0, 50) + Speed;
-            Particles[index].Velocity = new Vector2f((float)(Math.Cos(angle) * speed), (float)(Math.Sin(angle) * speed));
-            Particles[index].LifeTime = Time.FromMilliseconds((Randomizer.Next(0, lifeTime.AsMilliseconds())) + 1000);
-
-            // reset the position of the corresponding vertex
-            Vertices[(uint)index] = new Vertex(Emitter);
+            Particles.ForEach(p => p.Update(elapsed));
         }
 
         public void Draw(RenderTarget target, RenderStates states)
         {
-            // draw the vertex array
-            target.Draw(Vertices, states);
+            var array = new VertexArray(PrimitiveType.Points, (uint)Count);
+            for (int i = 0; i < Count; i++) array[(uint)i] = Particles[i].Vertices;
+            target.Draw(array, states);
         }
     }
 }
